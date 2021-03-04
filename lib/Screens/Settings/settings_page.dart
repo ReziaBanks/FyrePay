d4bf1fbd403182704398ba29504cee95767142b7
Rezia Banks <reziabanks@gmail.com>
2021-03-03 22:40:34 -0500

    Refactor Settings Page

--- lib/Screens/Settings/settings_page.dart
+++ lib/Screens/Settings/settings_page.dart
@@ -13,109 +13,80 @@ class _SettingsPageState extends State<SettingsPage> {
       appBar: AppBar(
         title: Text('Settings'),
       ),
-      body: Padding(
+      body: ListView(
         padding: EdgeInsets.only(top: 25, left: 25, right: 25, bottom: 50),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            //Account Details
-            Column(
-              children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Account Details',
-                    style: (Theme.of(context).textTheme.bodyText1).merge(
-                      TextStyle(fontWeight: FontWeight.bold)
-                    ),
-                  )
-                ),
-                SizedBox(height: 15),
+        children: [
+          //Account Details
+          Text(
+            'Account Details',
+            style: (Theme.of(context).textTheme.bodyText1)
+                .merge(TextStyle(fontWeight: FontWeight.bold)),
+          ),
+          SizedBox(height: 15),
 
-                AppContentTile(
-                  title: 'Change Email',
-                  onPressed: () {
-                  },
-                ),
+          AppContentTile(
+            title: 'Change Email',
+            onPressed: () {},
+          ),
 
-                SizedBox(height: 15),
+          SizedBox(height: 15),
 
-                AppContentTile(
-                  title: 'Security',
-                  onPressed: () {
-                  },
-                )
-              ],
-            ),
-            //Connection
-            Column(
-              children: [
-                Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Connection',
-                      style: (Theme.of(context).textTheme.bodyText1).merge(
-                          TextStyle(fontWeight: FontWeight.bold)
-                      ),
-                    )
-                ),
-                SizedBox(height: 15),
+          AppContentTile(
+            title: 'Security',
+            onPressed: () {},
+          ),
+          SizedBox(height: 35),
 
-                AppContentTile(
-                  title: 'Bank Account Info',
-                  onPressed: () {
-                  },
-                ),
+          //Connection
+          Text(
+            'Connection',
+            style: (Theme.of(context).textTheme.bodyText1)
+                .merge(TextStyle(fontWeight: FontWeight.bold)),
+          ),
+          SizedBox(height: 15),
 
-                SizedBox(height: 15),
+          AppContentTile(
+            title: 'Bank Account Info',
+            onPressed: () {},
+          ),
 
-                AppContentTile(
-                  title: 'Donation Settings',
-                  onPressed: () {
-                  },
-                )
-              ],
-            ),
-            //About
-            Column(
-              children: [
-                Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'About Green Apple Pay',
-                      style: (Theme.of(context).textTheme.bodyText1).merge(
-                          TextStyle(fontWeight: FontWeight.bold)
-                      ),
-                    )
-                ),
-                SizedBox(height: 15),
+          SizedBox(height: 15),
 
-                AppContentTile(
-                  title: 'Privacy Policy',
-                  onPressed: () {
-                  },
-                ),
+          AppContentTile(
+            title: 'Donation Settings',
+            onPressed: () {},
+          ),
+          //About
+          SizedBox(height: 35),
+          Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'About Green Apple Pay',
+                style: (Theme.of(context).textTheme.bodyText1)
+                    .merge(TextStyle(fontWeight: FontWeight.bold)),
+              )),
+          SizedBox(height: 15),
 
-                SizedBox(height: 15),
+          AppContentTile(
+            title: 'Privacy Policy',
+            onPressed: () {},
+          ),
 
-                AppContentTile(
-                  title: 'Terms Of Service',
-                  onPressed: () {
-                  },
-                ),
+          SizedBox(height: 15),
 
-                SizedBox(height: 15),
+          AppContentTile(
+            title: 'Terms Of Service',
+            onPressed: () {},
+          ),
 
-                AppContentTile(
-                  title: 'Logout',
-                  titleColor: Color.fromARGB(150, 255, 0, 0),
-                  onPressed: () {
-                  },
-                )
-              ],
-            ),
-          ],
-        ),
+          SizedBox(height: 15),
+
+          AppContentTile(
+            title: 'Logout',
+            titleColor: Color.fromARGB(150, 255, 0, 0),
+            onPressed: () {},
+          ),
+        ],
       ),
     );
   }
