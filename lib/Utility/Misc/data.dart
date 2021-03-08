b6540c0feced64a127b4783cbfe39cb1aed3f6da
Rezia Banks <reziabanks@gmail.com>
2021-03-08 13:33:00 -0500

    Update Organization List Page

--- lib/Utility/Misc/data.dart
+++ lib/Utility/Misc/data.dart
@@ -3,4 +3,7 @@ class AppData {
   static String bankConnectionText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit vel in cursus volutpat.';
   static String passwordResetText =
       'Enter your email address below, we’ll send you a verification code.';
+  static String organizationImageUrl1 = 'https://images.unsplash.com/photo-1615201427548-58316e349d8a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80';
+  static String organizationImageUrl2 = 'https://images.unsplash.com/photo-1615146919563-70db4a72f100?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80';
+  static String organizationImageUrl3 = 'https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1490&q=80';
 }
